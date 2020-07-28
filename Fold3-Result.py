58e4eca46a784b35ecfe9aaf1715c95131b7a462
abc1399f <68311743+abc1399f@users.noreply.github.com>
2020-07-28 08:34:06 +0430

    Update Fold3-Result.py

--- Fold3-Result.py
+++ Fold3-Result.py
@@ -352,42 +352,55 @@ def report(y_true, y_pred):
     '''
     list= [ 'X', '[CLS]', '[SEP]', 'O', 'I-loc', 'B-pers', 'I-pers', 'I-org', 'I-pro', 'B-pro','I-fac','B-fac', 'B-loc', 'B-org', 'B-event', 'I-event']
     '''
+    
     p, r, f= f1_score_report(y_true, y_pred, 4)
     print("precision, recall, F1 for I-loc", p, r, f)
-    
+
+
     p, r, f= f1_score_report(y_true, y_pred, 12)
     print("precision, recall, F1 for B-loc", p, r, f)     
-    
+
+        
     p, r, f= f1_score_report(y_true, y_pred, 5)
     print("precision, recall, F1 for B-pers", p, r, f)
+
     
     p, r, f= f1_score_report(y_true, y_pred, 6)
     print("precision, recall, F1 for I-pers", p, r, f)
+
     
     p, r, f= f1_score_report(y_true, y_pred, 7)
     print("precision, recall, F1 for I-org", p, r, f)
+
     
     p, r, f= f1_score_report(y_true, y_pred, 13)
     print("precision, recall, F1 for B-org", p, r, f)
+
     
     p, r, f= f1_score_report(y_true, y_pred, 8)
     print("precision, recall, F1 for I-pro", p, r, f)
+
     
     p, r, f= f1_score_report(y_true, y_pred, 9)
     print("precision, recall, F1 for B-pro", p, r, f) 
+
     
     p, r, f= f1_score_report(y_true, y_pred, 10)
     print("precision, recall, F1 for I-fac", p, r, f)
+
     
     p, r, f= f1_score_report(y_true, y_pred, 11)
     print("precision, recall, F1 for B-fac", p, r, f)   
+
  
        
     p, r, f= f1_score_report(y_true, y_pred, 14)
     print("precision, recall, F1 for B-event", p, r, f)   
+
     
     p, r, f= f1_score_report(y_true, y_pred, 15)
     print("precision, recall, F1 for I-event", p, r, f)  
+
     
 def f1_score(y_true, y_pred):
     '''
@@ -419,7 +432,6 @@ def f1_score(y_true, y_pred):
             f1=1.0
         else:
             f1=0
-
     return precision, recall, f1
 
 #%%
@@ -872,7 +884,6 @@ def evaluate(model, predict_dataloader, batch_size, epoch_th, dataset_name):
     #print(all_preds_convert)
     #report = classification_report(all_labels_convert, all_preds_convert,digits=4)
     report(np.array(all_labels), np.array(all_preds))
-    
     end = time.time()
     print('Epoch:%d, Acc:%.2f, Precision: %.2f, Recall: %.2f, F1: %.2f on %s, Spend:%.3f minutes for evaluation' \
         % (epoch_th, 100.*test_acc, 100.*precision, 100.*recall, 100.*f1, dataset_name,(end-start)/60.0))
@@ -913,7 +924,7 @@ for epoch in range(start_epoch, total_train_epochs):
             optimizer.zero_grad()
             global_step_th += 1
             
-        print("Epoch:{}-{}/{}, Negative loglikelihood: {} ".format(epoch, step, len(train_dataloader), neg_log_likelihood.item()))
+        #print("Epoch:{}-{}/{}, Negative loglikelihood: {} ".format(epoch, step, len(train_dataloader), neg_log_likelihood.item()))
     
     print('--------------------------------------------------------------')
     print("Epoch:{} completed, Total training's Loss: {}, Spend: {}m".format(epoch, tr_loss, (time.time() - train_start)/60.0))
